59dfdbd1030661d60dca34d5594e04962126c07a
JKOK005 <johan.kok@u.nus.edu>
2023-05-11 11:25:02 +0800

    XGBoost training example

--- adaptivestream/Models/Wrapper/XGBoostModelWrapper.py
+++ adaptivestream/Models/Wrapper/XGBoostModelWrapper.py
@@ -1,16 +1,16 @@
 import logging
+import tensorflow as tf
 from Buffer.Buffer import Buffer
 from Models.Wrapper.ModelWrapper import ModelWrapper
 from xgboost import XGBModel
 
 class XGBoostModelWrapper(ModelWrapper):
 	def __init__(self, 	xg_boost_model: XGBModel,
-						init_params: dict,
 						training_params: dict,
 						*args, **kwargs
 				):
 		super(XGBoostModelWrapper, self).__init__()
-		self.xg_boost_model 	= xg_boost_model(**init_params)
+		self.xg_boost_model 	= xg_boost_model
 		self.training_params 	= training_params
 		self.logger  			= logging.getLogger("XGBoostModelWrapper")
 		return
@@ -20,7 +20,6 @@ class XGBoostModelWrapper(ModelWrapper):
 			):
 		buffer_feat 	= buffer.get_data()
 		buffer_label 	= buffer.get_label()
-
 		self.xg_boost_model.fit(buffer_feat, buffer_label, **self.training_params)
 		return
 
