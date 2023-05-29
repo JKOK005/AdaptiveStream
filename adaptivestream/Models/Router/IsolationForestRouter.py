e6f57e320d23033ff712dc14eee20ca07bf4f5c7
JKOK005 <johan.kok@u.nus.edu>
2023-05-29 15:20:41 +0800

    Implementing isolation forest outlier detector

---
+++ adaptivestream/Models/Router/IsolationForestRouter.py
@@ -0,0 +1,39 @@
+import tensorflow as tf
+from Buffer.Buffer import Buffer
+from sklearn.ensemble import IsolationForest
+from Models.Router.Router import Router
+
+class IsolationForestRouter(Router):
+	def __init__(self, 	init_params: dict, 
+						*args, **kwargs
+		):
+		"""
+		Parameters follow suite sklearn.ensemble.IsolationForest class in Scikit [1]
+
+		[1]: https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.IsolationForest.html#sklearn.ensemble.IsolationForest.decision_function
+		"""
+		self.classifier = IsolationForest(**init_params)
+		return
+
+	def train(self, buffer: Buffer,
+					*args, **kwargs
+			):
+		feat = buffer.get_data()
+		self.classifier.fit(feat)
+		return
+
+	def permit_entry(self, 	input_X: tf.Tensor, 
+					  		*args, **kwargs
+			 		) -> bool:
+		"""
+		If class is 1 for input data, we permit entry.
+		"""
+		predicted_cls = self.classifier.predict(input_X)
+		return predicted_cls[0] == 1
+
+	def score(self, input_X: tf.Tensor,
+					*args, **kwargs
+			) -> float:
+		score = self.classifier.score_samples(input_X) + 1
+		normalized_score = 1 / (score + 1e-3)
+		return normalized_score.mean()
