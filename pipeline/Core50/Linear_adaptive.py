368892b4680b9ba7f03dadab5fc2298dd7cec853
Johan Kok <johan.kok@grabtaxi.com>
2023-11-27 15:40:17 +0800

    commit

--- pipeline/Core50/Linear_adaptive.py
+++ pipeline/Core50/Linear_adaptive.py
@@ -155,7 +155,7 @@ if __name__ == "__main__":
 		np.random.shuffle(train_dat)
 		ingested_counts  = 0
 
-		for each_training_dat in tqdm(np.array_split(train_dat, 8)):
+		for each_training_dat in tqdm(np.array_split(train_dat, 1)):
 			feats_as_tensor   = tf.convert_to_tensor(each_training_dat[:,0].tolist(), dtype = tf.float32)
 			labels_as_tensor  = tf.convert_to_tensor(each_training_dat[:,1].tolist(), dtype = tf.float32)
 			labels_as_tensor  = tf.reshape(labels_as_tensor, [len(labels_as_tensor), 1])
