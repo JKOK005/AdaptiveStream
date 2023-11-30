960fce0e276846e79741fa76e3be7196147b87b5
Johan Kok <johan.kok@grabtaxi.com>
2023-12-01 00:19:16 +0800

    commit

--- pipeline/Core50/Linear_adaptive.py
+++ pipeline/Core50/Linear_adaptive.py
@@ -155,10 +155,10 @@ if __name__ == "__main__":
 
 		train_dat 	= np.load(each_file, allow_pickle = True) # load
 		np.random.shuffle(train_dat)
-		train_dat   = train_dat[0:100]
+		train_dat   = train_dat
 		ingested_counts  = 0
 
-		for each_training_dat in tqdm(np.array_split(train_dat, 4)):
+		for each_training_dat in tqdm(np.array_split(train_dat, 2)):
 			feats_as_tensor   = tf.convert_to_tensor(each_training_dat[:,0].tolist(), dtype = tf.float32)
 			labels_as_tensor  = tf.convert_to_tensor(each_training_dat[:,1].tolist(), dtype = tf.float32)
 			labels_as_tensor  = tf.reshape(labels_as_tensor, [len(labels_as_tensor), 1])
@@ -167,7 +167,6 @@ if __name__ == "__main__":
 			ingested_counts += len(feats_as_tensor)
 			tf.keras.backend.clear_session()
 
-		break
 		logging.info(f"Total data ingested: {ingested_counts}")
 
 	import pickle
