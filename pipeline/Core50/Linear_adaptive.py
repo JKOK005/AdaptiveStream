89da5b32013986ee63cc7adb54f7563fe0fb9bfa
Johan Kok <johan.kok@grabtaxi.com>
2023-12-01 18:22:42 +0800

    commit

--- pipeline/Core50/Linear_adaptive.py
+++ pipeline/Core50/Linear_adaptive.py
@@ -55,7 +55,7 @@ def build_router(input_shape: (int), latent_dim: int):
 		},
 
 		training_params = {
-			"epochs" 		: 30, 
+			"epochs" 		: 20, 
 			"batch_size" 	: 32,
 			"verbose" 		: False,
 		}, 
@@ -120,7 +120,7 @@ if __name__ == "__main__":
 	base_model 	 	= 	build_net()
 
 	optimizer 		= 	tf.keras.optimizers.legacy.Adam(
-							learning_rate = 0.001,
+							learning_rate = 0.0001,
 						)
 
 	loss_fn 		=  	tf.keras.losses.SparseCategoricalCrossentropy(
@@ -132,7 +132,7 @@ if __name__ == "__main__":
 							optimizer 		= optimizer,
 							loss_fn 		= loss_fn,
 							training_params = {
-								"epochs" : 30,
+								"epochs" : 20,
 							}, 
 							training_batch_size = 32,
 						)
