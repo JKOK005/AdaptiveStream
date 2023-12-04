e3ae21cd2b4c824f0b7330b379f5aaca4b605cdb
Johan Kok <johan.kok@grabtaxi.com>
2023-12-04 22:02:48 +0800

    Commit

--- pipeline/Core50/Vanilla_lwf_train.py
+++ pipeline/Core50/Vanilla_lwf_train.py
@@ -41,13 +41,11 @@ class LwfLoss(tf.keras.losses.Loss):
 			self.prior_y_pred = y_pred
 
 		cur_loss 	= tf.keras.losses.sparse_categorical_crossentropy(y_true, y_pred)
-		prior_loss 	= tf.keras.losses.kullback_leibler_divergence(self.prior_y_pred, y_pred)
+		prior_loss 	= tf.keras.losses.kullback_leibler_divergence(y_pred, y_pred)
 
 		lwf_loss = 	(1 - self.lwf_alpha) * cur_loss + \
 					(self.lwf_alpha * (self.tmp ** 2)) * prior_loss
-
-		print(self.prior_y_pred)
-
+					
 		self.prior_y_pred = y_pred
 		return lwf_loss
 
