c2257b12a34c038bcf1f87fea295349415034d38
Johan Kok <johan.kok@grabtaxi.com>
2023-12-06 23:21:08 +0800

    Commit

--- pipeline/Core50/Vanilla_lwf_train.py
+++ pipeline/Core50/Vanilla_lwf_train.py
@@ -15,7 +15,7 @@ from tqdm import tqdm
 python3 pipeline/Core50/Vanilla_lwf_train.py \
 --net caffe \
 --train_dir /workspace/jupyter_notebooks/adaptive-stream/data/Core50/save/NI/train \
---save_path checkpoint/core50/caffenet/vanilla_lwf/alpha_0.5
+--save_path checkpoint/core50/caffenet/vanilla_lwf/alpha_0.7
 """
 
 def build_vgg_net():
@@ -78,7 +78,7 @@ if __name__ == "__main__":
 						learning_rate = 0.00005,
 					)
 
-	loss_fn 	= LwfLoss(tmp = 1.1, lwf_alpha = 0.5)
+	loss_fn 	= LwfLoss(tmp = 1.1, lwf_alpha = 0.7)
 
 	model 		= build_vgg_net() if args.net == "vgg" else build_caffe_net()
 
